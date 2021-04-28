4c3e5c351283c799802a5432844bc05efe944778
ActualTomato <narenragu@hotmail.com>
2021-04-27 19:41:08 -0700

    it stil wont commit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,14 +9,43 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed = 12f;
 
+    public float gravity = -9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    public float jumpHeight = 3f;
+
+    Vector3 velocity;
+    bool isGrounded;
+
     // Update is called once per frame
     void Update()
     {
+        //this part is the groundcheck to stop velocity from becoming comedy
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        //this is the part about WASD & jumping
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        // this is the part about gravity
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
