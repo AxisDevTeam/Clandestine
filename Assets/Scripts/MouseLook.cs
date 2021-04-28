f5e97b29297747a1c9f3fd580e81afc22834b3ef
ghettocat <catsitter2004@gmail.com>
2021-04-27 17:59:39 -0700

    Added mouse shit LOL and also wasd no gravity yet tho
    SIMPLE CAMERA FUCKS UP EVERYTHING

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+
+    public float mouseSensitivity = 100f;
+
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        //Locks the mouse in the center of the screen and makes it invisible 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        playerBody.Rotate(Vector3.up * mouseX);
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        //SIMPLE CAMERA CONTROL COMPLETELY FUCKS THIS SCRIPT!!! don't use it on 1st person cameras :thumbsup:
+        //naren you were right the unity character controller SUCKS
+
+    }
+}
